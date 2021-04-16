f0bc87754f2a6da00a3ed524f0d00376b0d098b4
cotebarrientos <cotebarrientos@gmail.com>
2021-04-16 01:14:19 +0100

    Update contexts.py
    I've updated contexts.py in order to access to the shopping cart
    saved in the user's session, also this functionality allows to
    to render inside cart.html the products selected by the user.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 # from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def cart_contents(request):
 
@@ -7,6 +10,18 @@ def cart_contents(request):
     subtotal = 0
     product_count = 0
     product_count_weight = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     total = subtotal
     
     context = {
