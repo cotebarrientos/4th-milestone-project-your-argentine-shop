bb3d9b1c5a011d03586fe6cc18b907cb67a97a4c
cotebarrientos <cotebarrientos@gmail.com>
2021-04-29 19:01:38 +0100

    Add basic Checkout functionality
    I've added the basic functionality in checkout in order to make
    payments using Stripe. In addition I installed the python-dotenv
    library, in purpose to create a virtual environment and have stored
    there all the secret keys that I will use to run stripe.

--- ms4/settings.py
+++ ms4/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -171,3 +175,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe Payments
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
