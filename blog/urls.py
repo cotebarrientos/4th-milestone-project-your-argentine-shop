b7bcfe628d3df7184209e66999d9cc44e96c28f5
cotebarrientos <cotebarrientos@gmail.com>
2021-05-10 20:00:53 +0100

    Add blog form and implement add post functionality
    I implemented the functionality to add blog posts to the website,
    using a form created specially for this task, and also this feature
    is exclusive for the super user or admin of the website, and to ensure
    that common users can not access this feature, I created some security
    mechanisms.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='blog'),
-    path('<post_id>/', views.post_detail, name='post_detail'),
+    path('int:<post_id>/', views.post_detail, name='post_detail'),
+    path('add/', views.add_post, name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import BlogPostForm
 
 
 def post_list(request):
@@ -19,6 +22,32 @@ def post_detail(request, post_id):
     template = 'blog/post_detail.html'
     context = {
         'post': post,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def add_post(request):
+    """ Add a post to the blog page """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            messages.success(request, 'Successfully added a new post!')
+            return redirect(reverse('post_detail', args=[post.id]))
+        else:
+            messages.error(request, 'Failed to add this post. Please ensure the form is valid.')
+    else:
+        form = BlogPostForm()
+
+    template = 'blog/add_post.html'
+    context = {
+        'form':form,
     }
 
     return render(request, template, context)
