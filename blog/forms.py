9aa4b16c179a03f036248b05ec34b8fe5c0f9c58
cotebarrientos <cotebarrientos@gmail.com>
2021-05-11 14:18:34 +0100

    Fix the image field
    I've created a custom widgets.py document, in order to render in a
    more pleasing to the eye the image field inside the form to add
    blog posts and edit them.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInputForPostImage
 from .models import Post
 
 
@@ -7,3 +8,11 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInputForPostImage)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
