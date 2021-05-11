f0df8bd88822828ae4478e62c5470050271b0199
cotebarrientos <cotebarrientos@gmail.com>
2021-05-11 19:22:55 +0100

    Update Home page
    The update made for the Home page is that when starting the website,
    in the Featured products and Combos section, these will render a
    random amount of products. To do this I added 2 variables in my
    function created in the views of this app, which are responsible for
    filtering all products on the website, showing only a certain amount
    of items.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product, Category
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page"""
+    FeaturedProducts = Product.objects.all().exclude(category__name='combos').order_by('?')[:8]
+    ComboProducts = Product.objects.filter(category__name='combos').order_by('?')[:4]
 
-    return render(request, 'home/index.html')
+    context = {
+        'FeaturedProducts': FeaturedProducts,
+        'ComboProducts': ComboProducts,
+    }
+
+    return render(request, 'home/index.html',context)
+
