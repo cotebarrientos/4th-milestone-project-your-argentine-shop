a51ab64aac4949ebbed11e466a6fc44ecfe003f8
cotebarrientos <cotebarrientos@gmail.com>
2021-05-19 19:55:16 +0100

    Implement customer reviews functionality
    I implemented the function of adding user reviews to the website. This
    feature is available to users who have a profile created, and they can
    do it by filling a form which contains the user's name, email, a field
    to write their comment and a dropdown to select their rating (the
    ranges are from 1 to 5 stars). Once the review is created, it will be
    under moderation of the super user, as a defense mechanism to avoid
    inappropriate comments or hate messages.

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Review
 from .forms import ReviewCommentForm
 
@@ -15,4 +16,39 @@ def review(request):
         'reviews': reviews,
         'review_form': review_form,
     }
+    return render(request, template, context)
+
+@login_required
+def write_review (request):
+    """ Add a customer review about the website """
+
+    new_review = None
+
+    if request.method == 'POST':
+        review_form = ReviewCommentForm(data=request.POST or None)
+        if review_form.is_valid():
+            name = request.POST.get( 'name')
+            email = request.POST.get( 'email')
+            rating = request.POST.get( 'rating')
+            comment = request.POST.get( 'comment')
+            new_review = Review.objects.create(
+                user=request.user,
+                name=name,
+                email=email,
+                rating=rating,
+                comment=comment
+            )
+            new_review.save()
+            messages.success(request, 'Successfully added your review. Your comment is awaiting moderation. Thank you so much!')
+            return redirect(reverse('review'))
+        else:
+            messages.error(request, 'Failed to add your review. Please ensure the form is valid.')
+    else:
+        review_form = ReviewCommentForm()
+
+    template = 'reviews/write_review.html'
+    context = {
+        'review_form': review_form,
+    }
+
     return render(request, template, context)
