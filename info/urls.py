e83fe7fc95111fd3b5db86b9360a9c7ed93a43ae
cotebarrientos <cotebarrientos@gmail.com>
2021-05-22 16:56:27 +0100

    Add privacy policy template
    I added privacy policy in order to provide assurance to users on how
    their personal information is handled within the website. I' ve also
    updated related urls in the footer as well as in the sign in template.

--- info/urls.py
+++ info/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('terms_of_service/', views.terms_of_service, name='terms_of_service'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- info/views.py
+++ info/views.py
@@ -4,3 +4,9 @@ def terms_of_service(request):
      """ A view to return the terms of service of the website"""
 
      return render(request, 'info/terms_of_service.html')
+
+
+def privacy_policy (request):
+    """ A view to return the privacy policy of the website""" 
+
+    return render(request,'info/privacy_policy.html')
