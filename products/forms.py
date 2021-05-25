7992ec6b49d01b592df6b860ccd3193a58eedc67
cotebarrientos <cotebarrientos@gmail.com>
2021-05-25 19:04:26 +0100

    Update products models and forms
    The new update consists in that inside the form to add or edit a
    product, there is a non-editable field (weight_unit) which has a
    default value which is "Kilogram". I have also added placeholders
    inside each form field.

--- products/forms.py
+++ products/forms.py
@@ -12,7 +12,19 @@ class ProductForm(forms.ModelForm):
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(ProductForm, self).__init__(*args, **kwargs)
+        
+        # Restrict editing this field within the form
+        self.fields['weight_unit'].widget.attrs['readonly'] = True
+
+        # Add placeholders into the Product Form
+        self.fields['product_code'].widget.attrs['placeholder'] = 'ALF-12U-CHO-660'
+        self.fields['name'].widget.attrs['placeholder'] = 'e.g. Alfajor HAVANNA Chocolate x12 660g'
+        self.fields['description'].widget.attrs['placeholder'] = 'Write the product description here ...'
+        self.fields['price'].widget.attrs['placeholder'] = '19.50'
+        self.fields['weight'].widget.attrs['placeholder'] = '0.660'
+
+        # Categories
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
